538a8ef9e0281a94b60fd6a2e34765b3dfb8ebeb
shubhamthorat07 <40167163+shubhamthorat07@users.noreply.github.com>
2018-08-27 19:58:52 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,64 @@
+package shubham.com.stopwatch;
+
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        runTimer();
+    }
+
+    public void onClickStart(View view){
+        running = true;
+    }
+
+    public void onClickStop(View view){
+        running = false;
+    }
+
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+  public void exit(){
+        
+  }
+
+    private void runTimer(){
+
+        final TextView timeView = (TextView)findViewById(R.id.time_View);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+
+                String time = String.format("%d:%02d:%02d" , hours , minutes , secs);
+                timeView.setText(time);
+
+                if (running) {
+                    seconds++ ;
+                }
+                handler.postDelayed(this, 1000);
+
+            }
+        });
+
+    }
+}
